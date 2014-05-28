9965a1afb5a93b89a720398a2e0d982e33320539
cong_yu <yu_cong880@hotmail.com>
2014-05-28 16:56:41 -0400

    node implementation

--- src/ca/mcgill/dist/databaseIndex/HTBLinkTree/common/HBLTInnerNode.java
+++ src/ca/mcgill/dist/databaseIndex/HTBLinkTree/common/HBLTInnerNode.java
@@ -1,5 +1,55 @@
 package ca.mcgill.dist.databaseIndex.HTBLinkTree.common;
 
-public class HBLTInnerNode {
+import java.util.Arrays;
+import java.util.List;
+
+
+public final class HBLTInnerNode implements HBLTNode{
+    final Object[] keys;
+    final long[] child;
+
+    public HBLTInnerNode(Object[] keys, long[] child) {
+        this.keys = keys;
+        this.child = child;
+    }
+
+    public HBLTInnerNode(Object[] keys, List<Long> child) {
+        this.keys = keys;
+        this.child = new long[child.size()];
+        for(int i=0;i<child.size();i++){
+            this.child[i] = child.get(i);
+        }
+    }
+
+
+    @Override 
+    public boolean isLeaf() { return false;}
+
+    @Override 
+    public Object[] keys() { return keys;}
+    
+    @Override 
+    public Object[] vals() { return null;}
+
+    @Override 
+    public Object highKey() {return keys[keys.length-1];}
+
+    @Override 
+    public long[] child() { return child;}
+
+    @Override 
+    public long next() {return child[child.length-1];}
+
+    @Override 
+    public String toString(){
+        return "Dir(K"+Arrays.toString(keys)+", C"+Arrays.toString(child)+")";
+    }
+
+	@Override
+	// Need to modify this
+	public HBLTNode nextNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/ca/mcgill/dist/databaseIndex/HTBLinkTree/common/HBLTLeafNode.java
+++ src/ca/mcgill/dist/databaseIndex/HTBLinkTree/common/HBLTLeafNode.java
@@ -1,5 +1,46 @@
 package ca.mcgill.dist.databaseIndex.HTBLinkTree.common;
 
-public class HBLTLeafNode {
+import java.util.Arrays;
 
+public final class HBLTLeafNode implements HBLTNode{
+    final Object[] keys;
+    final Object[] vals;
+    final long next;
+
+    HBLTLeafNode(Object[] keys, Object[] vals, long next) {
+        this.keys = keys;
+        this.vals = vals;
+        this.next = next;
+        assert(vals==null||keys.length == vals.length+2);
+    }
+
+    @Override 
+    public boolean isLeaf() { return true;}
+
+    @Override 
+    public Object[] keys() { return keys;}
+    
+    @Override 
+    public Object[] vals() { return vals;}
+
+    @Override 
+    public Object highKey() {return keys[keys.length-1];}
+
+    @Override 
+    public long[] child() { return null;}
+    
+    @Override 
+    public long next() {return next;}
+
+    @Override 
+    public String toString(){
+        return "Leaf(K"+Arrays.toString(keys)+", V"+Arrays.toString(vals)+", L="+next+")";
+    }
+
+	@Override
+	// Should be modified later
+	public HBLTNode nextNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
